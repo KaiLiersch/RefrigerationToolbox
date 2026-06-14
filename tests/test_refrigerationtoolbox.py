9f29c84fab34d66c7f46ed03d2903c9d9cfdd7e2
Kai Liersch <kailiersch@gmail.com>
2026-06-14 10:18:18 +0200

    Fixed ruff linter problems Testing CodeQL permission changes

--- tests/test_refrigerationtoolbox.py
+++ tests/test_refrigerationtoolbox.py
@@ -1,10 +1,13 @@
 """Tests for `refrigerationtoolbox` package."""
 
 import refrigerationtoolbox
-
 import refrigerationtoolbox.cycle as cycle
 
 
 def test_import():
     """Verify the package can be imported."""
     assert refrigerationtoolbox
+
+def test_cycle():
+    """Quick and dirty testcase"""
+    assert cycle.cycle("sdkh")
